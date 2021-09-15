8ac925726d2bb059c78d5e923497178936bf35a4
lizcraig <84337943+lizcraig@users.noreply.github.com>
2021-09-15 09:08:22 -0400

    h

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+const Counter = {
+    data() {
+      return {
+        counter: 0
+        foo: 0,
+        msg: "I hate D&S",
+        list: ["red", "green", "blue"]
+      }
+    }
+  }
+  
+  Vue.createApp(Offer).mount('#offerApp')
